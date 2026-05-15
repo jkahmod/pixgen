7744dcfcda4536abebb9963173b8ee95e9ffb95b
jkahmod <jakariaahmed.info@gmail.com>
2026-05-15 19:44:42 +0600

    change theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="en" data-theme="light"
       className={`${outfitFont.className} h-full antialiased`}
     >
       <body className="">
